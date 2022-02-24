35937fb5be19261c429b3e77996613874766dec0
Seth Allgire <seth.allgire@gmail.com>
2022-02-24 12:37:28 -0600

    mod 5 intro

---
+++ src/main/java/Finance.java
@@ -0,0 +1,7 @@
+public class Finance {
+    public final static String BEST_LOAN_RATES = "bestLoanRates";
+    public final static String SAVINGS_CALCULATOR = "savingsCalculator";
+    public final static String MORTGAGE_CALCULATOR = "mortgageCalculator";
+
+    public final static Map<String, String> = Map.of()
+}
