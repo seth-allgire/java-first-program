da63928d3e93886760f29e1fcf6725dd3c39ac02
Seth Allgire <seth.allgire@gmail.com>
2022-02-24 11:15:12 -0600

    mod 4 fix

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -1,5 +1,7 @@
 package com.h2;
 
+import java.text.DecimalFormat;
+
 public class MortgageCalculator {
     private long loanAmount;
     private int termInYears;
@@ -19,7 +21,7 @@ public class MortgageCalculator {
 
     private float getMonthlyInterestRate() {
         float interestRate = annualRate / 100;
-        return interestRate;
+        return interestRate / 12;
     }
 
     public void calculateMonthlyPayment() {
@@ -28,7 +30,21 @@ public class MortgageCalculator {
         int n = getNumberOfPayments();
 
         double M = P * (((r * Math.pow(1 + r, n))) / ((Math.pow((1 + r), n)) - 1));
-
         this.monthlyPayment = M;
+    }
+
+    public String toString() {
+        DecimalFormat df = new DecimalFormat("####0.00");
+        return "monthlyPayment: " + df.format(monthlyPayment);
+    }
+
+    public static void main(String[] args) {
+        long loanAmount = Long.parseLong(args[0]);
+        int termInYears = Integer.parseInt(args[1]);
+        float annualRate = Float.parseFloat(args[2]);
+
+        MortgageCalculator calculator = new MortgageCalculator(loanAmount, termInYears, annualRate);
+        calculator.calculateMonthlyPayment();
+        System.out.println(calculator.toString());
     }
 }
