1d62fd3cf480a2e96daadee0539efbf4754bbae5
Seth Allgire <seth.allgire@gmail.com>
2022-02-24 12:42:52 -0600

    mod 5

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -3,5 +3,5 @@ public class Finance {
     public final static String SAVINGS_CALCULATOR = "savingsCalculator";
     public final static String MORTGAGE_CALCULATOR = "mortgageCalculator";
 
-    public final static Map<String, String> = Map.of()
+    // public final static Map<String, String> = Map.of()
 }
